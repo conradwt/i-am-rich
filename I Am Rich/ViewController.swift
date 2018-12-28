7650146fcd7bb1dbc4df1b8f34120347f50aad7d
Conrad Taylor <conradwt@gmail.com>
2018-12-27 17:00:47 -0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Conrad Taylor on 12/27/18.
+//  Copyright © 2018 Conrad Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
